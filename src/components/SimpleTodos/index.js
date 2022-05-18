f37fdc26040282b38acaaa6bdff2757d62c0deba
Bharath Marla <marlabharathrgukt@gmail.com>
2022-05-18 15:27:50 +0000

    Todos app with delete button

---
+++ src/components/SimpleTodos/index.js
@@ -0,0 +1,76 @@
+// Write your code here
+import {Component} from 'react'
+
+import TodoItem from '../TodoItem'
+
+import './index.css'
+
+const initialTodosList = [
+  {
+    id: 1,
+    title: 'Book the ticket for today evening',
+  },
+  {
+    id: 2,
+    title: 'Rent the movie for tomorrow movie night',
+  },
+  {
+    id: 3,
+    title: 'Confirm the slot for the yoga session tomorrow morning',
+  },
+  {
+    id: 4,
+    title: 'Drop the parcel at Bloomingdale',
+  },
+  {
+    id: 5,
+    title: 'Order fruits on Big Basket',
+  },
+  {
+    id: 6,
+    title: 'Fix the production issue',
+  },
+  {
+    id: 7,
+    title: 'Confirm my slot for Saturday Night',
+  },
+  {
+    id: 8,
+    title: 'Get essentials for Sunday car wash',
+  },
+]
+
+class SimpleTodos extends Component {
+  state = {
+    todosList: initialTodosList,
+  }
+
+  onDeleteButton = id => {
+    const {todosList} = this.state
+    const upDatedTodoList = todosList.filter(each => each.id !== id)
+    this.setState({todosList: upDatedTodoList})
+  }
+
+  render() {
+    const {todosList} = this.state
+
+    return (
+      <div className="bg-container">
+        <div className="sub-container">
+          <h1 className="heading">Simple Todos</h1>
+          <ul className="unorder-list">
+            {todosList.map(eachList => (
+              <TodoItem
+                initialTodosList={eachList}
+                key={eachList.id}
+                deleteTodo={this.onDeleteButton}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SimpleTodos
